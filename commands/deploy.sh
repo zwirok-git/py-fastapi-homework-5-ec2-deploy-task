63216258e192224a2d63e6ff5045163783744a6d
zwirok-git <oleksijprokopenko@gmail.com>
2026-07-02 18:18:24 +0300

    update deploy

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -10,7 +10,7 @@ handle_error() {
 }
 
 # Navigate to the application directory
-cd /home/ubuntu/src/mate-fastapi-homework-5 || handle_error "Failed to navigate to the application directory."
+cd /home/ubuntu/src/mate-fastapi-homework-5-ec2-deploy-task || handle_error "Failed to navigate to the application directory."
 
 # Fetch the latest changes from the remote repository
 echo "Fetching the latest changes from the remote repository..."
